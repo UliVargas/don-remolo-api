e64ba1285c5c1a69424ba1ff13d7ca02550315cc
Ulises Vargas <uli.vargas02@gmail.com>
2023-05-23 17:23:09 -0600

    Agrego proteccion a getAllProducts

--- src/routes/v1/products.routes.ts
+++ src/routes/v1/products.routes.ts
@@ -1,8 +1,9 @@
 import Router from 'express-promise-router'
 import { getAllProducts } from '../../controllers/products.controllers'
+import { Auth } from '../../middleware'
 
 const router = Router()
 
-router.get('/', getAllProducts)
+router.get('/', Auth, getAllProducts)
 
 export default router
