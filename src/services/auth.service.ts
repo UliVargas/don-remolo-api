5ac647d02b69491d7d26c8fa76454c7ea3032276
Ulises Vargas <uli.vargas02@gmail.com>
2023-05-23 17:49:43 -0600

    cambio nobre de loginService

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,22 +1,15 @@
 import { User } from '@prisma/client'
 import { prisma } from '../../prisma/client'
-import { BCCompare, BCHash } from '../utilities/bcrypt'
 import { JWTSign } from '../utilities/jwt'
 
-export const LoginService = async ({ email, password }: { email: string, password: string}) => {
-  const user = await prisma.user.findFirst({
-    where: {
-      email
-    }
-  })
-
+export const CreateTokenService = async (user: User): Promise<string> => {
   return JWTSign({
     user: user.id,
     role: user.role
   })
 }
 
-export const CreateUser = async (data: { email: string, password: string }) => {
+export const CreateUser = async (data: { email: string, password: string }): Promise<User> => {
   return prisma.user.create({
     data: {
       ...data
