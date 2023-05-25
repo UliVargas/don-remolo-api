bf55e6171ef04114bea0b67ad0cc7bfed32b24ac
julian moreno <julianmoreno242@gmail.com>
2023-05-25 11:25:48 -0500

    Agregado ruta para getAllOrders

--- src/routes/v1/orders.routes.ts
+++ src/routes/v1/orders.routes.ts
@@ -1,9 +1,10 @@
 import Router from 'express-promise-router'
 import { Auth } from '../../middleware'
-import { createOrder } from '../../controllers/orders.controllers'
+import { createOrder, getAllOrders } from '../../controllers/orders.controllers'
 
 const router = Router()
 
+router.get('/', Auth, getAllOrders)
 router.post('/', createOrder)
 
 export default router
