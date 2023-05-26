47b8e033f8c6140b920c2289cb0c5c78ad03e3f2
julian moreno <julianmoreno242@gmail.com>
2023-05-26 15:12:36 -0500

    Se agrego controlador para obtener una order

--- src/controllers/orders.controllers.ts
+++ src/controllers/orders.controllers.ts
@@ -1,10 +1,15 @@
 import { NextFunction, Request, Response } from 'express'
 import addOrders from '../useCases/orders/addOrders'
 import getAll from '../useCases/orders/getAll'
+import getOneOrder from '../useCases/orders/getOneOrder'
 
 export const createOrder = async (req:Request, res:Response) => {
   res.status(201).json(await addOrders(req.body))
 }
 export const getAllOrders = async (req:Request, res:Response, next:NextFunction) => {
   res.status(200).json(await getAll())
+}
+export const getOneOrderById = async (req:Request, res:Response, next:NextFunction) => {
+  const { params } = req
+  res.status(200).json(await getOneOrder(params.idOrder))
 }
