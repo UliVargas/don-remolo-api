9fc9fa76143daacbb95a1594eb17c2088db0d50f
julian moreno <julianmoreno242@gmail.com>
2023-05-26 12:07:04 -0500

    Se agrego controlador para actualizar orden

--- src/controllers/orders.controllers.ts
+++ src/controllers/orders.controllers.ts
@@ -1,10 +1,18 @@
 import { NextFunction, Request, Response } from 'express'
 import addOrders from '../useCases/orders/addOrders'
 import getAll from '../useCases/orders/getAll'
+import editOrder from '../useCases/orders/updateOrder'
 
 export const createOrder = async (req:Request, res:Response) => {
   res.status(201).json(await addOrders(req.body))
 }
 export const getAllOrders = async (req:Request, res:Response, next:NextFunction) => {
   res.status(200).json(await getAll())
+}
+export const updateOrder = async (req:Request, res:Response, next:NextFunction) => {
+  const data = {
+    idOrder: req.params.idOrder,
+    order: req.body
+  }
+  res.status(200).json(await editOrder(data))
 }
