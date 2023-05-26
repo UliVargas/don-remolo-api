9a0cfb886140f4cd5e15ca8448b309069f35c283
julian moreno <julianmoreno242@gmail.com>
2023-05-26 15:13:40 -0500

    Se añadio ruta y requerimiento de autenticacion para obtener una ruta.

--- src/routes/v1/orders.routes.ts
+++ src/routes/v1/orders.routes.ts
@@ -1,10 +1,11 @@
 import Router from 'express-promise-router'
 import { Auth } from '../../middleware'
-import { createOrder, getAllOrders } from '../../controllers/orders.controllers'
+import { createOrder, getAllOrders, getOneOrderById } from '../../controllers/orders.controllers'
 
 const router = Router()
 
 router.get('/', Auth, getAllOrders)
+router.get('/:idOrder', Auth, getOneOrderById)
 router.post('/', Auth, createOrder)
 
 export default router
