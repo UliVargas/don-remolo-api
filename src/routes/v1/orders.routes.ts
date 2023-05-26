817676592d0456ab11fe7815d301dbcfe19e6d27
julian moreno <julianmoreno242@gmail.com>
2023-05-26 12:12:31 -0500

    Se agrego  la ruta y el requisito de autenticacion para manejar la actualizacion de una orden

--- src/routes/v1/orders.routes.ts
+++ src/routes/v1/orders.routes.ts
@@ -1,10 +1,11 @@
 import Router from 'express-promise-router'
 import { Auth } from '../../middleware'
-import { createOrder, getAllOrders } from '../../controllers/orders.controllers'
+import { createOrder, getAllOrders, updateOrder } from '../../controllers/orders.controllers'
 
 const router = Router()
 
 router.get('/', Auth, getAllOrders)
 router.post('/', Auth, createOrder)
+router.patch('/update/:idOrder', Auth, updateOrder)
 
 export default router
