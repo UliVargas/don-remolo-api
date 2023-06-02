8918f87352ff4b08beb287df3d2e1f6808d4fefc
julian moreno <julianmoreno242@gmail.com>
2023-06-02 10:37:51 -0500

    Se agrego validacion en la ruta para crear ordenes

--- src/routes/v1/orders.routes.ts
+++ src/routes/v1/orders.routes.ts
@@ -1,12 +1,12 @@
 import Router from 'express-promise-router'
 import { Auth } from '../../middleware'
 import { createOrder, getAllOrders, getOneOrderById, updateOrder } from '../../controllers/orders.controllers'
-
+import OrderValidation from '../../validations/functions/order'
 const router = Router()
 
 router.get('/', Auth, getAllOrders)
 router.get('/:idOrder', Auth, getOneOrderById)
-router.post('/', Auth, createOrder)
+router.post('/', Auth, OrderValidation.create, createOrder)
 router.patch('/update/:idOrder', Auth, updateOrder)
 
 export default router
