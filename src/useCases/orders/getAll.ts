ae7ee9c3820f30ed2a369d42d96ab7c9a0355a99
julian moreno <julianmoreno242@gmail.com>
2023-06-16 11:28:33 -0500

    Se añadio parametro de querys con su tipado

--- src/useCases/orders/getAll.ts
+++ src/useCases/orders/getAll.ts
@@ -1,5 +1,6 @@
 import { GetAllOrdersService } from '../../services/order.service'
+import { QueryType } from '../../types/orderTypes'
 
-export default async () => {
-  return GetAllOrdersService()
+export default async (querys:QueryType) => {
+  return GetAllOrdersService(querys)
 }
