295ab0a98f3831a8b5b4ba4da960cf4774c23b90
julian moreno <julianmoreno242@gmail.com>
2023-06-17 09:36:31 -0500

    Se refactorizo funcion getAllOrders en los controladores

--- src/controllers/orders.controllers.ts
+++ src/controllers/orders.controllers.ts
@@ -4,21 +4,34 @@ import getAll from '../useCases/orders/getAll'
 import editOrder from '../useCases/orders/updateOrder'
 import getOneOrder from '../useCases/orders/getOneOrder'
 
-export const createOrder = async (req:Request, res:Response) => {
+export const createOrder = async (req: Request, res: Response) => {
   res.status(201).json(await addOrders(req.body))
 }
 
-export const getAllOrders = async (req:Request, res:Response, next:NextFunction) => {
-  const querys = {
-    status: req.query.status ? req.query.status.toString() : null,
-    offset: req.query.offset ? Number(req.query.offset) : 1,
-    limit: req.query.limit ? Number(req.query.limit) : 10,
-    date: req.body.date ? req.body.date.toString() : ''
-  }
-  res.status(200).json(await getAll(querys))
+export const getAllOrders = async (req: Request, res: Response, next: NextFunction) => {
+  const {
+    status,
+    offset = 1,
+    limit = 10
+
+  } = req.query
+  const { date } = req.body
+
+  res.status(200).json(await getAll({
+    status,
+    offset: Number(offset),
+    limit: Number(limit),
+    date
+
+  } as {
+    status: string
+    offset: number
+    limit: number
+    date: string
+  }))
 }
 
-export const updateOrder = async (req:Request, res:Response, next:NextFunction) => {
+export const updateOrder = async (req: Request, res: Response, next: NextFunction) => {
   const data = {
     idOrder: req.params.orderId,
     order: req.body
@@ -26,7 +39,7 @@ export const updateOrder = async (req:Request, res:Response, next:NextFunction) 
   res.status(200).json(await editOrder(data))
 }
 
-export const getOneOrderById = async (req:Request, res:Response, next:NextFunction) => {
+export const getOneOrderById = async (req: Request, res: Response, next: NextFunction) => {
   const { params } = req
   res.status(200).json(await getOneOrder(params.orderId))
 }
